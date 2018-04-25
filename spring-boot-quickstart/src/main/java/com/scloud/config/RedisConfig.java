1e1d9388d2477a3148133d8f8351b68acf8853f1
zhangcaiming <zhangcm3581@163.com>
2018-04-26 00:21:16 +0800

    集成Redis

---
+++ spring-boot-quickstart/src/main/java/com/scloud/config/RedisConfig.java
@@ -0,0 +1,60 @@
+package com.scloud.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+
+    /**
+     *  redis.conf 配置文件修改
+     *      1. 注释掉bind 127.0.0.1可以使所有的ip访问redis
+     *      2. protected-mode no
+     *      3. requirepass redis!123 （开启密码登录）
+     *
+     */
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheManager redisCacheManager = RedisCacheManager.builder(connectionFactory).build();
+        return redisCacheManager;
+    }
+
+    /**
+     *  防止redis入库序列化乱码的问题
+     */
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // 使用Jackson2JsonRedisSerializer 替换默认序列化（jdkSerializeable）
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        // 设置value的序列化规则和 key的序列化规则
+        redisTemplate.setKeySerializer(new StringRedisSerializer()); //key序列化
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);  //value序列化
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+}
---
+++ spring-boot-quickstart/src/test/java/com/scloud/redis/RedisTest.java
@@ -0,0 +1,62 @@
+package com.scloud.redis;
+
+import com.scloud.domain.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    public void test() throws Exception{
+        stringRedisTemplate.opsForValue().set("aaa", "111");
+        Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+    }
+
+    @Test
+    public void testObj() throws Exception {
+        User user=new User();
+        user.setName("andy");
+        user.setAge(23);
+        user.setId(1L);
+        ValueOperations<String, User> operations=redisTemplate.opsForValue();
+        operations.set("com.neox", user);
+        Thread.sleep(1000);
+        //redisTemplate.delete("com.neo.f");
+        boolean exists=redisTemplate.hasKey("com.neox");
+        if(exists){
+            System.out.println("exists is true");
+        }else{
+            System.out.println("exists is false");
+        }
+        Assert.assertEquals("andy", operations.get("com.neox").getName());
+    }
+
+    @Test
+    public void testExist() {
+        User user = (User) redisTemplate.opsForValue().get("com.neox");
+        System.out.println(user);
+        Assert.assertEquals("andy", user.getName());
+    }
+
+
+
+
+
+}
