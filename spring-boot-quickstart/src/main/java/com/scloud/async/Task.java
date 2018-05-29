5607c57adc58401ae9c420ada5bcb12a7f1eadc0
zhangcaiming <zhangcm3581@163.com>
2018-05-29 23:06:46 +0800

    使用@Async实现异步调用

---
+++ spring-boot-quickstart/src/main/java/com/scloud/async/Task.java
@@ -0,0 +1,54 @@
+package com.scloud.async;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+import java.util.concurrent.Future;
+
+/**
+ * Create by andy on 2018/5/29
+ * 使用@Async实现异步调用
+ */
+@Component
+public class Task {
+
+    public static Random random = new Random();
+
+    @Async
+    public Future<String> doTaskOne() throws Exception{
+        System.out.println("开始做任务一");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(1000));
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务一，耗时：" + (end - start) + "毫秒");
+        return new AsyncResult<>("任务一完成");
+    }
+
+    @Async
+    public Future<String> doTaskTow() throws Exception{
+        System.out.println("开始做任务二");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(1000));
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务二，耗时：" + (end - start) + "毫秒");
+        return new AsyncResult<>("任务二完成");
+    }
+
+    @Async
+    public Future<String> doTaskThree() throws Exception{
+        System.out.println("开始做任务三");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(1000));
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务三，耗时：" + (end - start) + "毫秒");
+        return new AsyncResult<>("任务三完成");
+    }
+
+
+
+
+
+
+}
