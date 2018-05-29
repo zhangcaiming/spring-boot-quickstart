304f21e9311a65b55be9c4884624b7fd6f19466e
zhangcaiming <zhangcm3581@163.com>
2018-05-29 23:47:01 +0800

    使用AOP同一处理web请求日志

---
+++ spring-boot-quickstart/src/main/java/com/scloud/aspect/WebLogAspect.java
@@ -0,0 +1,60 @@
+package com.scloud.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Arrays;
+
+/**
+ * Web层日志切面
+ * <p>
+ * Create by andy on 2018/5/29
+ */
+@Aspect
+@Order(5)
+@Component
+@Slf4j
+public class WebLogAspect {
+
+    ThreadLocal<Long> startTime = new ThreadLocal<>();
+
+    @Pointcut("execution(public * com.scloud.web..*.*(..))")
+    public void webLog() {}
+
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        startTime.set(System.currentTimeMillis());
+
+        // 接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = (HttpServletRequest) attributes.getRequest();
+
+        // 记录下请求内容
+        log.info("URL : " + request.getRequestURI());
+        log.info("HTTP_METHOD : " + request.getMethod());
+        log.info("IP : " + request.getRemoteAddr());
+        log.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        log.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容
+        log.info("RESPONSE : " + ret);
+        log.info("SPEND TIME : " + (System.currentTimeMillis() - startTime.get()));
+    }
+
+
+
+
+}
